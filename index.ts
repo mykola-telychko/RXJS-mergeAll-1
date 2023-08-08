68f58ee27fddf9e1c16afbffa6cf474c9917cd17
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-08 07:23:49 +0300

    pin

--- index.ts
+++ index.ts
@@ -1,14 +1,18 @@
 import { map, mergeAll } from 'rxjs/operators';
 import { of } from 'rxjs';
 
+// https://www.learnrxjs.io/learn-rxjs/operators/combination/mergeall
+
 // Collect and subscribe to all observables
 
 const samplePromise = (val) =>
-  new Promise((resolve) => setTimeout(() => resolve(`Result: ${val}`), 2000));
+  new Promise((resolve) =>
+    setTimeout(() => resolve(`Promise El_: ${val}`), 2000)
+  );
 //emit 1,2,3
-const src = of(1, 2, 3);
+const srcList = of(1, 2, 3);
 
-const exmpl = src.pipe(
+const exmpl = srcList.pipe(
   //map each value to promise
   map((val) => samplePromise(val)),
   //emit result from source
